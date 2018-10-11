e7597d198c5731681313e8bd6f2148863e7efdb9
balodi <balodi@gmail.com>
2018-10-11 20:31:34 +0530

    added route description to get the medicine text

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,10 @@
 const Hapi = require('hapi');
+const vision = require('@google-cloud/vision');
+const path = require('path');
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = 'secrets/index.json';
+
+const client = new vision.ImageAnnotatorClient();
 
 const Port = 8080;
 const server = new Hapi.Server();
@@ -7,13 +13,35 @@ server.connection({
   port: Port,
 });
 
-server.route({
-  method: 'GET',
-  path: '/ping',
-  handler: (request, response) => {
-    response({ statusCode: 200, message: 'pong' });
-  },
-});
+server.route([
+  {
+    method: 'GET',
+    path: '/ping',
+    handler: (request, response) => {
+      response({ statusCode: 200, message: 'pong' });
+    },
+  }, {
+    method: 'GET',
+    path: '/description',
+    handler: (request, response) => {
+      client
+        .textDetection(path.resolve(__dirname, '../resources/test.jpeg'))
+        .then((results) => {
+          const textAnnotationsDescriptions = [];
+          const text = results[0].textAnnotations;
+          text.forEach((textAnnotations) => {
+            const Lines = textAnnotations.description.split('\n');
+            Lines.forEach((line) => {
+              textAnnotationsDescriptions.push(line);
+            });
+          });
+          response({ statusCode: 200, textAnnotationsDescriptions });
+        })
+        .catch((err) => {
+          console.error('ERROR:', err);
+        });
+    },
+  }]);
 
 
 const start = async () => {
