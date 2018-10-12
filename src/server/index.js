eb1c097302859559e827f907ce1ea3588686a414
balodi <balodi@gmail.com>
2018-10-12 06:09:13 +0530

    added post routes one for error check and other to send image contents of uploaded image

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
 const vision = require('@google-cloud/vision');
 const posTagger = require('wink-pos-tagger');
+const fs = require('fs');
 const path = require('path');
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = 'secrets/index.json';
@@ -28,6 +29,44 @@ server.route([
     method: 'GET',
     path: '/description',
     handler: (request, response) => {
+      client
+        .textDetection(path.resolve(__dirname, '../resources/test.jpeg'))
+        .then((results) => {
+          const textAnnotationsDescriptions = [];
+          const text = results[0].textAnnotations;
+          // text.forEach((textAnnotations) => {
+          const textAnnotations = text[0];
+          const Lines = textAnnotations.description.split('\n');
+          Lines.forEach((line) => {
+            const tokens = tagger.tagSentence(line);
+            let newLine = '';
+            tokens.forEach((token) => {
+              if (posTokens.indexOf(token.pos) !== -1) {
+                newLine = `${newLine} ${token.value}`;
+              }
+            });
+            if (newLine.length !== 0) { textAnnotationsDescriptions.push(newLine); }
+          });
+          // });
+          response({ statusCode: 200, textAnnotationsDescriptions });
+        })
+        .catch((err) => {
+          console.error('ERROR:', err);
+        });
+    },
+  }, {
+    method: 'POST',
+    path: '/error',
+    handler: (request, response) => {
+      response(request.payload);
+    },
+  }, {
+    method: 'POST',
+    path: '/image',
+    handler: (request, response) => {
+      const writeStream = fs.createWriteStream(path.resolve(__dirname, '../resources/test.jpeg'), { flags: 'w' });
+      writeStream.write(request.payload.file);
+      writeStream.end();
       client
         .textDetection(path.resolve(__dirname, '../resources/test.jpeg'))
         .then((results) => {
